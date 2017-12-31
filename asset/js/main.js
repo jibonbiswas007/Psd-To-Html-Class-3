8f3f689c974d4339fb79a951c55610e9b425d9ba
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2018-01-01 00:57:21 +0600

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+        $('.video-play-btn').magnificPopup()
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
